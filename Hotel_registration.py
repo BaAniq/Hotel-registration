49842fffa7c61a2236b0cc026fbc340562c381b4
BaAniq <basia.anisimowicz@gmail.com>
2023-07-10 20:44:05 +0200

    Convert actions in the loops to functions

--- Hotel_registration.py
+++ Hotel_registration.py
@@ -1,48 +1,48 @@
 # TYPY ZAGNIEŻDŻONE
 
-osoba1=[217,'Orlando','Bloom',25,'mężczyzna']
-osoba2=[217,'Katy','Perry',25,'kobieta']
-osoba3=[218,'Angelina','Jolie',51,'kobieta']
-osoba4=[218,'Brad','Pitt',46,'mężczyzna']
-osoba5=[117,'Henryk','Sienkiewicz',18,'mężczyzna']
+guest1=[217, 'Orlando', 'Bloom', 25, 'mężczyzna']
+guest2=[217, 'Katy', 'Perry', 25, 'kobieta']
+guest3=[218, 'Angelina', 'Jolie', 51, 'kobieta']
+guest4=[218, 'Brad', 'Pitt', 46, 'mężczyzna']
+guest5=[117, 'Henryk', 'Sienkiewicz', 18, 'mężczyzna']
 
-listaG=[osoba1,osoba2,osoba3,osoba4,osoba5]
+list_guest=[guest1, guest2, guest3, guest4, guest5]
 
 
 z=1
 # dodawanie gości
 while z==1:
-    wybor=int(input('Co chcesz zrobić:\
-            \n  1 - wyświetlić listę gości\
-            \n  2 - zameldować gości\
-            \n  3 - wymeldować gości \n'))
+    choice=int(input('What do you want to do?:\
+            \n  1 - show the Guest List\
+            \n  2 - check in guest \
+            \n  3 - check out guest \n'))
 
 
-    if wybor==1:
-          for d in listaG:
-              print(d)
-    elif wybor==2:
-        imie=input('Imię:   ')
-        imie=imie.capitalize()
-        nazw=input('Nazwisko:   ')
-        nazw=nazw.capitalize()
-        wiek=int(input('Wiek:   '))
-        plec=input('Płeć:   ')
-        pokoj=int(input('Pokój: '))
-        gosc=[pokoj,imie,nazw,wiek,plec]
-        listaG.append(gosc)
+    if choice==1:
+          for guest in list_guest:
+              print(guest)
+    elif choice==2:
+        name=input('Name:   ')
+        name=name.capitalize()
+        surname=input('Surname:   ')
+        surname=surname.capitalize()
+        age=int(input('Age:   '))
+        sex=input('Sex:   ')
+        room=int(input('Room: '))
+        guest=[room, name, surname, age, sex]
+        list_guest.append(guest)
         
-    elif wybor==3:
+    elif choice==3:
         x=0
-        for i in range(1,len(listaG)+1):
+        for i in range(1, len(list_guest) + 1):
         
-            print(f'{i}.',listaG[x])
+            print(f'{i}.', list_guest[x])
             
             x+=1
-        gusu=int(input('Którego gościa chcesz wymeldować?:  '))
-        r=gusu-1
-        listaG.pop(r)
+        guest_del=int(input('Which guest would you like to check out?:  '))-1
 
-    z=int(input('Czy chcesz kontynuować:    \
-            \n 1 - Tak\
-            \n 2 - Nie \n '))
+        list_guest.pop(guest_del)
+
+    z=int(input('Do you want to continue:    \
+            \n 1 - Yes\
+            \n 2 - No \n '))
